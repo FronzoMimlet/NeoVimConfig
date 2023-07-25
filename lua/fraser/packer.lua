0514895dde9978b15f86db27d7c72eb0fc511620
Fraser <frasermiller0@gmail.com>
2023-07-25 18:32:15 +0100

    added lsp-zero

--- lua/fraser/packer.lua
+++ lua/fraser/packer.lua
@@ -16,6 +16,26 @@ return require('packer').startup(function(use)
   run = function()
        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
        ts_update()
-  end,
-    }
+  end,}
+
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.api.nvim_command, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+  }
   end)
